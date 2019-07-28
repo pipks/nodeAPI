16c4d363078c01cd5b4e10348fb7c65e9940257f
Pipks Holess <pipksholess@protonmail.com>
2019-07-28 12:28:22 +0300

    Create Movie Schema and Router

---
+++ models/Movie.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema();
+
+//created Movie Schema
+const MovieSchema = new Schema({
+    director_id : Schema.Types.ObjectId,
+    title : {
+        type: String,
+        required: true
+    },
+    category : String,
+    country : String,
+    year : Number,
+    imdb_score: Number,
+    date : {
+        type : Date,
+        default : Date.now,
+    }
+});
+
+//export Movie Schema
+module.exports = mongoose.model('movie', MovieSchema);
