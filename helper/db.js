2640dd4445c92c648ce2c0f122e1f7180010f7c6
Pipks Holess <pipksholess@protonmail.com>
2019-08-05 22:33:06 +0300

    start scripts

---
+++ helper/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+
+module.exports = () => {
+
+    mongoose.connect('mongodb://movie_user:abcd1234@ds147436.mlab.com:47436/heroku_hf6sn4sw', 
+    { useNewUrlParser: true, useFindAndModify: false });
+
+    mongoose.connection.on('open', () => {
+        //console.log('MongoDB: Connected');
+    });
+
+    mongoose.connection.on('error', (err) => {
+        console.log('MongoDB: Error', err);
+    });
+    //global promise yapısını kullanacağımızı belirttik.
+    mongoose.Promise = global.Promise;
+}
